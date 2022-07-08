8138d47dcf17339cac4658e65945c828cb5baa5d
Tristan <tristandecertaines@gmail.com>
2022-07-08 12:34:31 +0100

    experiments with pacf

--- pairs_trading.py
+++ pairs_trading.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 import yfinance as yf
+from statsmodels.tsa.stattools import pacf
+from statsmodels.graphics.tsaplots import plot_pacf
 
 from utils.kernels import Kernel
 from utils.estimation import estimate_parameters_tvAR_p
@@ -15,7 +17,7 @@ if __name__ == '__main__':
     # DATA & SPREAD
     
     # BTC-USD / ETH-USD
-    data = yf.download("BTC-USD ETH-USD", period="6mo", interval="1h")
+    data = yf.download("BTC-USD ETH-USD", period="1y", interval="1h")
     data = data['Close']
     data = data.dropna()
     data = np.log(1 + data.pct_change()) # log returns
@@ -48,23 +50,31 @@ if __name__ == '__main__':
     time_series = data['spread'].to_numpy()
     T = time_series.shape[0]
     b_T = T ** (-1/5)
+    p = 1
     print(f"{T=}")
     print(f"{b_T=}")
 
-    theta_hat = estimate_parameters_tvAR_p(time_series=time_series, p=1, u_list=u_list, kernel=Kernel("epanechnikov"), bandwidth=b_T)
+    theta_hat = estimate_parameters_tvAR_p(time_series=time_series, p=p, u_list=u_list, kernel=Kernel("epanechnikov"), bandwidth=b_T)
     alpha_hat = theta_hat[:, :-1, :]
     sigma_hat = theta_hat[:, -1, :]
 
-
-    # PLOT
+    ## PLOT
     fig = plt.figure(constrained_layout=True)
-    subfigs = fig.subfigures(2, 1)
+    subfigs = fig.subfigures(3, 1)
+    # time series
     sns.lineplot(data=data, ax=subfigs[0].subplots(1, 1))
-    axs = subfigs[1].subplots(1, 2)
-    axs[0].plot(u_list, alpha_hat[:, 0], label="alpha")
-    axs[1].plot(u_list, sigma_hat, label="sigma")
-    for ax in axs.flat:
-        ax.legend()
+    # alpha
+    axs = subfigs[1].subplots(1, p) if p > 1 else [subfigs[1].subplots(1, p)]
+    for i in range(p):
+        axs[i].plot(u_list, alpha_hat[:, i, :])
+        axs[i].set_title(f"alpha_{i+1}")
+    # sigma
+    ax = subfigs[2].subplots(1, 1)
+    ax.plot(u_list, sigma_hat)
+    ax.set_title("sigma")
+
+    fig2, ax2 = plt.subplots()
+    pacf, conf_int = pacf(time_series, nlags=10, alpha=0.05)
+    plot_pacf(time_series, ax2, lags=np.arange(10), alpha=0.05)
+    
     plt.show()
-
-
