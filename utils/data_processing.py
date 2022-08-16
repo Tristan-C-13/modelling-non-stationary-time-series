ae446fb52524ab4e8baeb650914cb3c9460dd2c2
Tristan <tristandecertaines@gmail.com>
2022-08-16 10:30:50 +0100

    load_spread_btc_eth function

--- utils/data_processing.py
+++ utils/data_processing.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 import datetime as dt
 import logging
+import os
 
 import yfinance as yf
 
@@ -55,6 +56,54 @@ def download_and_prepare_data(symbol1:str, symbol2:str, **kwargs) -> pd.DataFram
     start_datetime = data_df.index[0]
     end_datetime = data_df.index[-1]
     logging.info(f"Data downloaded: from {start_datetime} to {end_datetime}")
+    return data_df
+
+
+def load_spread_btc_eth(start: str = None, end: str = None) -> pd.DataFrame:
+    """
+    Returns a pd.DataFrame containing close prices of ETH/USDT and BTC/USDT as well as their spread (raw spread + spread of log returns)
+
+    --- parameters
+    - start (str): start date in format YYYY-MM-DD
+    - end (str): end date in format YYYY-MM-DD
+    """
+    # Load csv if it already exists:
+    if os.path.exists('data/data-binance.csv'):
+        data_df = pd.read_csv('data/data-binance.csv', index_col='date')
+    else:
+        # Load historical data
+        btc_df = pd.read_csv('data/Binance_BTCUSDT_1h.csv', sep=';', parse_dates=True, index_col='date', usecols=['date', 'close'])
+        btc_df = btc_df.rename(columns={'close': 'btc_close'})
+        btc_df = btc_df.iloc[::-1] # Reverse to have recent values at the end
+        eth_df = pd.read_csv('data/Binance_ETHUSDT_1h.csv', sep=';', parse_dates=True, index_col='date', usecols=['date', 'close'])
+        eth_df = eth_df.rename(columns={'close': 'eth_close'})
+        eth_df = eth_df.iloc[::-1]
+        
+        # Compute log returns
+        btc_df['btc_log_returns'] = np.log(1 + btc_df['btc_close'].pct_change())
+        eth_df['eth_log_returns'] = np.log(1 + eth_df['eth_close'].pct_change())
+
+        # Compute spreads
+        data_df = pd.concat([btc_df, eth_df], axis=1)
+        data_df = data_df.dropna()
+        data_df['spread'] = data_df['btc_close'] - data_df['eth_close']
+        data_df['spread_log_returns'] = data_df['btc_log_returns'] - data_df['eth_log_returns']
+
+        # Save
+        data_df.to_csv("data/data-binance.csv", index=True, index_label='date')
+        logging.info("Data successfully saved.")
+
+    # Slice dataframe
+    if start is not None:
+        data_df = data_df.loc[start:, :]
+    if end is not None:
+        data_df = data_df.loc[:end, :]
+
+    # Logging info
+    start_datetime = data_df.index[0]
+    end_datetime = data_df.index[-1]
+    logging.info(f"Data loaded from {start_datetime} to {end_datetime}")
+
     return data_df
 
 
